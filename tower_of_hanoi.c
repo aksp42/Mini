3bc86f152bc90f8d867fe9ad5aa715f65df15dbe
AkaVortex <akanksha24d@gmail.com>
2025-03-01 21:58:45 +0530

    Update tower_of_hanoi.c

--- tower_of_hanoi.c
+++ tower_of_hanoi.c
@@ -1,21 +1,3 @@
-// #include <stdio.h>
-// void toh (int n, char A , char B , char C ){
-//    if (n==1){
-//     printf("Move disk 1 from %c to %c\n",A,C);}
-//     else {
-//         toh(n-1,A,C,B);
-//         printf("Move disk %d from %c to %c\n",n,A,C);
-//         toh (n-1,B,A,C);}
-// }
-// int main(){
-//     int m =3;
-//     char from = 'A';
-//     char to = 'B';
-//     char use = 'C';
-//     toh(m,from,to,use);
-//     return 0;
-// }
-
 #include <stdio.h>
 void towerOfHanoi (int n , char from , char use , char to){
     if(n==1){
